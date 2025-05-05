9905efa0f5729136bfde0ceb15bf1631d52115a5
Oliver <114983621+oblassgit@users.noreply.github.com>
2025-05-05 12:37:37 +0200

    added license text to notifier.py

--- notifier.py
+++ notifier.py
@@ -1,3 +1,19 @@
+# refurbished steam deck notifier - notifies when refurbished steam deck is in stock
+#  Copyright (C) <2025>  <Oliver Blass>
+# 
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from time import gmtime, strftime
 import requests
 from discord_webhook import DiscordWebhook
